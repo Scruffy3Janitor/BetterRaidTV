009557a372dd99ce7bd1340ae43cb304a20cbe97
Scruffy3Janitor <scruffy3janitor@gmail.com>
2021-06-13 20:12:24 -0400

    Update code.js
    updated to reflect new "no-reload" argument passed with raid

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 chrome.tabs.onUpdated.addListener(function(tabId,obj,tab) {
-    if (tab.url.indexOf("?referrer=raid")!==-1 && tab.url.indexOf("twitch")!==-1) {
+    if (tab.url.indexOf("referrer=raid")!==-1 && tab.url.indexOf("twitch.tv")!==-1) {
         console.log("Redirecting out of raid");
         chrome.tabs.update(tabId, {
-            url: tab.url.substring(0,tab.url.indexOf('?'))
+            url: tab.url.substring(0,tab.url.indexOf('referrer=raid')) + tab.url.substring(tab.url.indexOf('referrer=raid')+13)
         });
     };
 });
